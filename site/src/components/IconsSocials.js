b50509aabd0df7b8878bdcdc29f6598ef0de8a40
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2022-01-02 00:44:49 -0500

    security fix
    added `rel="noopener noreferrer" to socials icons`

--- site/src/components/IconsSocials.js
+++ site/src/components/IconsSocials.js
@@ -3,7 +3,7 @@ import '../App.css';
 
 export function GithubIcon() {
     return (
-        <a id="social-icon" href="https://github.com/Ti1mmy" target="_blank">
+        <a id="social-icon" href="https://github.com/Ti1mmy" target="_blank" rel="noopener noreferrer">
             <Github />
         </a>
     )
@@ -12,7 +12,7 @@ export function GithubIcon() {
 
 export function LinkedinIcon() {
     return (
-        <a id="social-icon" href="http://www.linkedin.com/in/Timothy-Zheng21" target="_blank">
+        <a id="social-icon" href="http://www.linkedin.com/in/Timothy-Zheng21" target="_blank" rel="noopener noreferrer">
             <Linkedin />
         </a>
     )
@@ -20,7 +20,7 @@ export function LinkedinIcon() {
 
 export function TwitterIcon() {
     return (
-        <a id="social-icon" href="https://twitter.com/intent/user?screen_name=Ti1mmyy" target="_blank">
+        <a id="social-icon" href="https://twitter.com/intent/user?screen_name=Ti1mmyy" target="_blank" rel="noopener noreferrer">
             <Twitter />
         </a>
     )
