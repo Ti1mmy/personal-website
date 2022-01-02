080cbfa779c8791aeb14607ba976bfa0b700d710
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2022-01-01 23:59:23 -0500

    attempt fix
    since trying https://github.com/laravel-mix/laravel-mix/issues/1016#issuecomment-352746059 didn't work, removing and adding stuff back slowly

--- site/src/App.js
+++ site/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <MyName />
-      <ContactWidgets /> 
+      {/* <ContactWidgets />  */}
     </div>
   );
 }
--- site/src/components/Titles.js
+++ site/src/components/Titles.js
@@ -19,7 +19,7 @@ export function ContactWidgets() {
             <GithubIcon />
             <LinkedinIcon />
             <TwitterIcon />
-            {/* <EmailButton /> */}
+            <EmailButton />
         </div>
     )
 }
