e22209687f29f942caa4e2a794cd79783ecca564
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2022-01-02 00:04:09 -0500

    revert back to last working commit

---
+++ tempsite/site/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import { MyName, ContactWidgets } from './components/Titles.js'
+
+function App() {
+  return (
+    <div className="App">
+      <MyName />
+      {/* <ContactWidgets />  */}
+    </div>
+  );
+}
+
+export default App;
