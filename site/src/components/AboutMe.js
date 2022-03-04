a8e52aff273013f528bbe28f0b1262dd2f1a7620
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2022-03-03 22:44:17 -0500

    Update AboutMe.js

--- site/src/components/AboutMe.js
+++ site/src/components/AboutMe.js
@@ -11,7 +11,7 @@ export function AboutMe() {
                 I'm an avid tech enthusiast, and I also enjoy learning as much as I can through firsthand experience. I work hard to be reliable to my peers, and I love helping others.            </div>
             <div>&#8203;</div>
             <div>
-                I love &#127992; playing badminton, &#129374; cooking, &#128200; managing my finances, and &#127950;&#65039; Formula 1.
+                I love &#127992; playing badminton, &#129374; cooking, and &#128200; managing my finances.
             </div>
         </div>
     )
